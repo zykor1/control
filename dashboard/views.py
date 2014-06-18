b121330268f6d2f58e446c050b7ed9e472724b6f
zykor <enrique.wx@gmail.com>
2014-06-18 13:32:03 -0500

    mostrando correctamente la imagen y solo se mostrara la de facebook

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='/login')
 def dashboard(request):
-	return render(request, 'dashboard.html', {},
+	back = request.user.social_auth.get(provider="facebook")
+	return render(request, 'dashboard.html', { 'uid': back.uid },
         content_type="text/html")
